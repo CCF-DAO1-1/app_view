05471f14c22ef9d619b8a0acee23098390e3bea7
JLer <jlerxky@live.com>
2026-01-08 16:06:42 +0800

    feat: add ProposalUriQuery struct and update get function to validate proposal query parameter

--- src/api/meeting.rs
+++ src/api/meeting.rs
@@ -1,10 +1,16 @@
 use color_eyre::eyre::eyre;
 use common_x::restful::{
-    axum::{extract::State, response::IntoResponse},
+    axum::{
+        extract::{Query, State},
+        response::IntoResponse,
+    },
     ok,
 };
 use sea_query::{Expr, ExprTrait, Order, PostgresQueryBuilder};
 use sea_query_sqlx::SqlxBinder;
+use serde::Deserialize;
+use utoipa::IntoParams;
+use validator::Validate;
 
 use crate::{
     AppView,
@@ -12,10 +18,24 @@ use crate::{
     lexicon::meeting::{Meeting, MeetingRow, MeetingState},
 };
 
-#[utoipa::path(get, path = "/api/meeting")]
-pub async fn get(State(state): State<AppView>) -> Result<impl IntoResponse, AppError> {
+#[derive(Debug, Default, Validate, Deserialize, IntoParams)]
+#[serde(default)]
+pub struct ProposalUriQuery {
+    #[validate(length(min = 1))]
+    pub proposal: String,
+}
+
+#[utoipa::path(get, path = "/api/meeting", params(ProposalUriQuery))]
+pub async fn get(
+    State(state): State<AppView>,
+    Query(query): Query<ProposalUriQuery>,
+) -> Result<impl IntoResponse, AppError> {
+    query
+        .validate()
+        .map_err(|e| AppError::ValidateFailed(e.to_string()))?;
     let (sql, values) = Meeting::build_select()
         .and_where(Expr::col(Meeting::State).eq(MeetingState::Scheduled as i32))
+        .and_where(Expr::col(Meeting::ProposalUri).eq(&query.proposal))
         .order_by(Meeting::Created, Order::Desc)
         .build_sqlx(PostgresQueryBuilder);
 
