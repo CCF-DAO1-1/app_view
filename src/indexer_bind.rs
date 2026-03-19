e30423c1a7f2dbc0c5e44fb32bb1e73b2a2d70c1
JLer <jlerxky@live.com>
2026-03-19 16:28:53 +0800

    optim: get bind_list at height

--- src/indexer_bind.rs
+++ src/indexer_bind.rs
@@ -26,6 +26,23 @@ pub async fn query_by_to(url: &str, to: &str) -> Result<Value> {
         })?
 }
 
+pub async fn query_by_to_at_height(url: &str, to: &str, height: u64) -> Result<Value> {
+    reqwest::Client::new()
+        .get(format!("{url}/by_to_at_height/{to}/{height}"))
+        .header("Content-Type", "application/json; charset=utf-8")
+        .timeout(Duration::from_secs(5))
+        .send()
+        .await
+        .map_err(|e| eyre!("call indexer failed: {e}"))?
+        .json::<Value>()
+        .await
+        .map(|r| {
+            r.pointer("/data")
+                .cloned()
+                .ok_or_eyre("missing data field in indexer response")
+        })?
+}
+
 pub async fn query_by_from(url: &str, from: &str) -> Result<Value> {
     reqwest::Client::new()
         .get(format!("{url}/by_from/{from}"))
@@ -50,7 +67,11 @@ pub async fn get_weight(
     ckb_addr: &str,
     until_block_number: Option<u64>,
 ) -> Result<u64> {
-    let from_list = crate::indexer_bind::query_by_to(indexer_bind_url, ckb_addr).await?;
+    let from_list = if let Some(until_block_number) = until_block_number {
+        query_by_to_at_height(indexer_bind_url, ckb_addr, until_block_number).await?
+    } else {
+        query_by_to(indexer_bind_url, ckb_addr).await?
+    };
     let mut weight =
         get_nervos_dao_deposit(ckb_client, ckb_net, ckb_addr, until_block_number).await?;
 
